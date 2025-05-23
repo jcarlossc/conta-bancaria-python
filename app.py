9871cf2a95ff04c5fec546f6bdc62251ff7c9d68
jcarlossc <josecscosta@fac.pe.senac.br>
2025-05-23 16:14:10 -0300

    Classe Extrato adicionada.

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from banco.usuario.PessoaFisica import PessoaFisica
-from banco.usuario.PessoaJuridica import PessoaJuridica
-from banco.conta.ContaCorrente import ContaCorrente
-from banco.conta.ContaPoupanca import ContaPoupanca
 
-pf2 = PessoaJuridica('carlos', 'carlos@email.com', 987654)
-cc2 = ContaPoupanca(pf2)
-
-pf1 = PessoaFisica('jose', 'jose@email.com', 123456)
-cc1 = ContaCorrente(pf1)
-
-cc1.depositar(500)
-cc1.sacar(100)
-cc1.transferir(100, cc2)
-print(cc1.historico.acessa_transacoes())
---
+++ banco/extrato/Extrato.py
@@ -0,0 +1,24 @@
+class Extrato:
+    """Classe que representa o extrato das contas. 
+
+    Atributos:
+        None.
+    """
+    def __init__(self):
+        pass
+        
+    def acessar_extrato(self, conta):
+        """Imprime extrato.
+
+        Retorna:
+            None.
+        """
+        print("---------------------------------------- EXTRATO ----------------------------------------")
+        print(f"TIPO DE CONTA: {conta.tipo_conta()}")
+        print(f"TITULAR: {conta.usuario.nome}")
+        print(f"CPF: {conta.usuario.cpf}")
+        print(f"SALDO: R$ {round(conta.saldo, 2)}")
+        print("LISTA DE TRANSAÇÕES")
+        for transacao in conta.historico.transacoes:
+            print(transacao)
+        print("------------------------------------------ FIM ------------------------------------------")
